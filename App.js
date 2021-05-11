a7f499436e6de3975918d43e4df13631100725f7
unknown <a.egret@laposte.net>
2021-05-11 10:13:47 +0200

    exercie flexbox 4

--- App.js
+++ App.js
@@ -4,18 +4,73 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <View style={styles.containerParent}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.containerHorizontal}>
+        <View style={styles.containerHorizontal1}>
+        </View>
+        <View style={styles.containerHorizontal2}>
+        </View>
+        <View style={styles.containerHorizontal3}>
+        </View>
+      </View>
+
+      <View style={styles.containerVertical}>
+        <View style={styles.containerVertical1}>
+        </View>
+        <View style={styles.containerVertical2}>
+        </View>
+      </View>
+      </View>
+      <View style={styles.containerFooter}>
+      </View>
+      
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerParent: {
+    flex:1,
   },
+  container: {
+    flex:1 ,
+    backgroundColor: 'grey',
+    flexDirection:'row',
+  },
+  containerHorizontal: {
+    flex: 3,
+  },
+  containerHorizontal1: {
+    flex:2,
+    backgroundColor: 'purple',
+  },
+  containerHorizontal2: {
+    flex: 2,
+    backgroundColor:'yellow',
+  },
+  containerHorizontal3: {
+    flex: 2,
+    backgroundColor:'pink',
+  },
+  containerVertical: {
+    flex: 1,
+    flexDirection:'row',
+  },
+  containerVertical1: {
+    flex: 2,
+    backgroundColor:'red',
+  },
+  containerVertical2: {
+    flex: 2,
+    backgroundColor:'blue',
+  },
+  containerFooter: {
+    flex: 1,
+    backgroundColor:'grey',
+  }
+
+
 });
